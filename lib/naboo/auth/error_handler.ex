a9a34cf25c50e9b446f79c2f9cd1bc6524386f3d
Dave Muirhead <dave@brsg.io>
2020-06-13 08:34:14 -0400

    remove debug logging

--- lib/naboo/auth/error_handler.ex
+++ lib/naboo/auth/error_handler.ex
@@ -5,9 +5,6 @@ defmodule Naboo.Auth.ErrorHandler do
 
   @impl Guardian.Plug.ErrorHandler
   def auth_error(conn, {type, reason}, opts) do
-    IO.inspect(conn)
-    IO.puts("ErrorHandler.auth_error - type=#{type} reason=#{reason} opts=")
-    IO.inspect(opts)
     body = Jason.encode!(%{error: to_string(type)})
     send_resp(conn, 401, body)
   end
