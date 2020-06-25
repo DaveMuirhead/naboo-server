52f8c0cd383ad615c0d9d2711a667d1d55455e60
Dave Muirhead <dave@brsg.io>
2020-06-25 18:23:48 -0400

    cope with nil user

--- lib/naboo/auth/verify-active.ex
+++ lib/naboo/auth/verify-active.ex
@@ -7,17 +7,30 @@ defmodule Naboo.Auth.VerifyActive do
   def init(opts), do: opts
 
   def call(conn, opts) do
-    with user <- Guardian.Plug.current_resource(conn) do
-      case user.active do
-        true ->
-          conn
-        false ->
-          conn
-          |> Pipeline.fetch_error_handler!(opts)
-          |> apply(:auth_error, [conn, {:user_inactive, "user account is inactive"}, opts])
-          |> halt()
-      end
+    user = Guardian.Plug.current_resource(conn)
+    active_or_halt(conn, opts, user)
+  end
+
+  defp active_or_halt(conn, opts, nil) do
+    dont_continue(conn, opts)
+  end
+
+  defp active_or_halt(conn, opts, user) do
+    case user.active do
+      true -> do_continue(conn, opts)
+      false -> dont_continue(conn, opts)
     end
+  end
+
+  defp do_continue(conn, _opts) do
+    conn
+  end
+
+  defp dont_continue(conn, opts) do
+    conn
+    |> Pipeline.fetch_error_handler!(opts)
+    |> apply(:auth_error, [conn, {:user_inactive, "user account is inactive"}, opts])
+    |> halt()
   end
 
 end
